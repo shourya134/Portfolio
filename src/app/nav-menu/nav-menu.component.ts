6913fa97d4e0a11cfa639eaaa9d793537fbd4888
Shourya Sharma <50227800+shourya134@users.noreply.github.com>
2023-07-01 04:15:26 -0400

    nav made responsive

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class NavMenuComponent {
 
+  public toggle : boolean = false;
+              
+//define your method
+  clickEvent(){
+    //if you just want to toggle the class; change toggle variable.
+    this.toggle = !this.toggle;       
+  }
 }
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav")
+const burger = document.getElementById("burger-menu")
+
+if(burger!= null && ul != null){
+  burger.addEventListener("click",() =>{
+    ul.classList.add("show")
+
+})}
